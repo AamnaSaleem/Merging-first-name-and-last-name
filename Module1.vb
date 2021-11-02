fb67cf5105b8c51c8ba2116b9814da42ed97a9d4
AamnaSaleem <93542393+AamnaSaleem@users.noreply.github.com>
2021-11-02 07:05:45 +0500

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim FirstName, SecondName, FN, LN, LenSTR As String
+        Dim SpaPos As Integer
+        Console.Write("Enter First Name: ")
+        FirstName = Console.ReadLine
+        Console.Write("Enter Second Name: ")
+        SecondName = Console.ReadLine
+        SpaPos = InStr(FirstName, " ")
+        FN = Left(FirstName, SpaPos - 1)
+        SpaPos = InStr(SecondName, " ")
+        lenSTR = Len(SecondName)
+        LN = Right(SecondName, lenSTR - SpaPos)
+        Console.WriteLine(FN & " " & LN)
+        Console.ReadKey()
+
+
+
+
+    End Sub
+
+End Module
